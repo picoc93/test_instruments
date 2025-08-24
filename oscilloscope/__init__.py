1ee3765e19767b7ea126db23d661c1aba0a975e8
picoc93 <pietro.coccia.ts@gmail.com>
2025-08-24 15:54:22 +0200

    user_manual

---
+++ oscilloscope/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+__all__ = [
+    'gw_instek_gds_806s'
+]
+
+from . import *
---
+++ oscilloscope/gw_instek_gds_806s.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+import serial
+import io
+from enum import IntEnum
+import time;
+
