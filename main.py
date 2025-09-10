19d05c3166c5157a7aff3967ac7618cae4c06e03
picoc93 <99282785+picoc93@users.noreply.github.com>
2025-09-10 10:47:08 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -9,9 +9,10 @@ def main():
 
     test_bench.connect_oscilloscope('COM11')
     test_bench.connect_function_generator('COM21')
-    test_bench.connect_psu('COM22')
+    test_bench.connect_power_supply('COM22')
 
     test_handler=test_manager.Test_Manager(test_bench)
+    test_handler.set_voltage_list([3.8])
     test_handler.add_test_to_stack(test.Instrument_Commands_Test(test_bench))
     test_handler.add_test_to_stack(test.Frequency_Response_Test(test_bench))
 
