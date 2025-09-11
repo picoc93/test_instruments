169cb96c0a7f7f604626d1a0ac34879e513f9fe1
picoc93 <99282785+picoc93@users.noreply.github.com>
2025-09-11 16:04:00 +0200

    Add files via upload

--- test_manager/test_manager.py
+++ test_manager/test_manager.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python3
+from enum import Enum
+import time
 
 ###############################################################################
-class Test_Manager:
+class External_Voltage(float,Enum):
+    ELV=3.0
+    LV=3.3
+    NV=3.8
+    HV=4.0
+    EHV=4.4
+
+###############################################################################
+class Test_Manager: #run tests, collect measures, insert measures in DB
 
     def __init__(self,bench):
-        self.test_list=[]
         self.bench=bench
+        self.voltage_list=[]
+        self.test_list=[]
 
     def add_test_to_stack(self,test):
         self.test_list.append(test)
@@ -13,7 +24,23 @@ class Test_Manager:
     def remove_test_from_stack(self,test):
         self.test_list.remove(test)
 
+    def add_external_voltage_to_stack(self,voltage):
+        self.voltage_list.append(voltage)
+    
+    def remove_external_voltage_to_stack(self,voltage):
+        self.voltage_list.remove(voltage)
+
     def run_test_stack(self):
-        for test in self.test_list:
-            test.run()
+        if self.test_list:
+            for test in self.test_list:
+                start_time=time.start()
+                test.run()
+                stop_time=time.stop()
+
+    def run_test_stack_voltage_loop(self):
+        if self.voltage_list:
+            for voltage in self.voltage_list:
+                self.bench.power_supply.set_voltage(voltage)
+                self.run_test_stack()
+
 
