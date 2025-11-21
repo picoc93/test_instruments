c0607e4a258e025a2ca74960ed30fb809894984c
picoc93 <99282785+picoc93@users.noreply.github.com>
2025-11-21 13:34:50 +0100

    Add files via upload

--- test_manager/test_manager.py
+++ test_manager/test_manager.py
@@ -1,37 +1,29 @@
 #!/usr/bin/env python3
-
+import time
+import json
 import time
 
 class Test_Manager: #run tests, collect measures, insert measures in DB
 
     def __init__(self,bench):
         self.bench=bench
-        self.voltage_list=[]
-        self.test_list=[]
-
-    def add_test_to_stack(self,test):
-        self.test_list.append(test)
-    
-    def remove_test_from_stack(self,test):
-        self.test_list.remove(test)
-
-    def add_external_voltage_to_stack(self,voltage):
-        self.voltage_list.append(voltage)
-    
-    def remove_external_voltage_to_stack(self,voltage):
-        self.voltage_list.remove(voltage)
+        self.test_stack=[]
 
     def run_test_stack(self):
-        if self.test_list:
-            for test in self.test_list:
-                start_time=time.start()
-                test.run()
-                stop_time=time.stop()
+        self.test_manager_initialization()
+        self.test_manager_loop()
+        self.test_manager_termination()
 
-    def run_test_stack_voltage_loop(self):
-        if self.voltage_list:
-            for voltage in self.voltage_list:
-                self.bench.power_supply.set_voltage(voltage)
-                self.run_test_stack()
+    def test_manager_initialization(self):
+        print('ciao')
+        self.start_time=time.time()
+
+        return
+
+    def test_manager_loop(self):
+        return
+
+    def test_manager_termination(self):
+        self.stop_time=time.time()
 
 
