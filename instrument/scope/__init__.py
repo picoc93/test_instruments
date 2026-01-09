0c11084939d483a6055c27f830f5e0b6bb0331aa
picoc93 <99282785+picoc93@users.noreply.github.com>
2026-01-09 10:52:05 +0100

    Delete instrument/scope directory

--- instrument/scope/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-__all__ = [
-    'scope_factory.py'
-]
-
-from .scope_factory import *
-
--- instrument/scope/scope_factory.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-
-class Oscilloscope_Factory:
-
-    def create_Oscilloscope(oscilloscope_id):
-        if oscilloscope_id == "gw_instek_gds_806s":
-            return Owon()
-        else:
-            raise ValueError(f"Unknown instrument type: {oscilloscope_id}")
